04430b6d6e8286651d44c9dce4437046cb602d7a
Rayhan0Islam0Shagor <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2021-03-24 18:15:08 +0600

    blog page go on

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Home from "./Components/Home/Home";
 
+// import react-bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../Layout";
+import BlogPage from "./BlogPage/BlogPage";
 
 const Home = () => {
   return (
     <Layout>
+      <BlogPage />
       <h1>home</h1>
     </Layout>
   );
