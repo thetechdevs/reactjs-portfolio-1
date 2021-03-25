bc0c6c61584185aef833faf46c7600b9efd37d36
SONJOY BARMON <sonjoybarmon19@gmail.com>
2021-03-26 04:03:57 +0600

    completed navmenu sticky navber and design increment blogpage

--- src/Components/Home/BlogPage/BlogPage.jsx
+++ src/Components/Home/BlogPage/BlogPage.jsx
@@ -1,39 +1,57 @@
 import { useState } from "react";
 // import bootstrap components
-import { Row, Col, Card } from "react-bootstrap";
+// import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 // import assets
 import BlogImage from "../../../assets/image/blog.png";
 // import CSS
 import "./BlogPage.css";
 // import dummyData
 import blogPosts from "../../../dummyData/DummyData";
-import { Container } from "@material-ui/core";
+import { Card, CardContent, Container, Typography } from "@material-ui/core";
 import Layout from "../../Layout";
 
 const BlogPage = () => {
   return (
     <Layout>
-      <Container>
-        <p className="heading_line">The Blog</p>
-        <Row className="align-items-center">
-          <Col lg={6} md={6} sm={12} xs={12}>
-            <div className="image_container blog_card">
-              <img className="image-fluid" src={BlogImage} alt="" />
-            </div>
-          </Col>
-          <Col lg={4} md={6} sm={12} xs={12}>
-            <small className="text-muted">March 12, 2020</small>
-            <p className="blog_heading blog_card">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
-              distinctio
-            </p>
-          </Col>
-        </Row>
-        <BlogPosts />
-      </Container>
+      {/* <div style={{ backgroundColor: "#E5E6EB" }}> */}
+      <div>
+        <Container>
+          <Typography
+            variant="h4"
+            className="heading_line"
+            style={{ marginBottom: "15px" }}
+          >
+            The <span style={{ color: "#f9bb00" }}> Blog</span>
+          </Typography>
+          <Row className="align-items-center">
+            <Col lg={6} md={6} sm={12} xs={12}>
+              <div className="image_container blog_card">
+                <img className="image-fluid" src={BlogImage} alt="" />
+              </div>
+            </Col>
+            <Col lg={4} md={6} sm={12} xs={12}>
+              <div>
+                <small className="text-muted">March 12, 2020</small>
+                <Typography
+                  variant="h5"
+                  className="blog_heading blog_card"
+                  style={{ margin: "15px 0" }}
+                >
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur, distinctio
+                </Typography>
+              </div>
+            </Col>
+          </Row>
+          <div style={{ padding: "20px 0 30px 0" }}>
+            <BlogPosts />
+          </div>
+        </Container>
+      </div>
     </Layout>
   );
 };
@@ -46,7 +64,22 @@ const BlogPosts = () => {
         {blogs.map((blog) => (
           <Col lg={4} md={6} sm={12} xs={12}>
             <div className="mt-5 blog_card">
-              <Card style={{ backgroundColor: "#E5E6EB", border: "none" }}>
+              {/* <Card style={{ backgroundColor: "#E5E6EB", border: "none" }}> */}
+              <Card style={{ border: "none" }}>
+                <div style={{ overflow: "hidden" }}>
+                  <img className="blog_image" src={blog.image} alt="" />
+                </div>
+                <CardContent style={{ padding: "20px" }}>
+                  <small className="text-muted">{blog.date}</small>
+                  <Typography variant="h6" className="blog_title">
+                    {blog.blog_title.substring(0, 25) + ".."}
+                  </Typography>
+                  <Typography variant="subtitle1" className="text-muted">
+                    {blog.description.substring(0, 120) + ".."}
+                  </Typography>
+                </CardContent>
+              </Card>
+              {/* <Card style={{ backgroundColor: "#E5E6EB", border: "none" }}>
                 <div style={{ overflow: "hidden" }}>
                   <img className="blog_image" src={blog.image} alt="" />
                 </div>
@@ -59,7 +92,7 @@ const BlogPosts = () => {
                     {blog.description.substring(0, 120) + ".."}
                   </p>
                 </Card.Body>
-              </Card>
+              </Card> */}
             </div>
           </Col>
         ))}
--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -1,82 +1,199 @@
-import React from "react";
+import { React, createRef, useState, useEffect } from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { Button } from "@material-ui/core";
 import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
+import "./Navbar.scss";
 
-const NavBar = () => {
+const NavBar = ({ children, sticky = false, className, ...rest }) => {
+  const [isSticky, setIsSticky] = useState(false);
+  const ref = createRef();
+
+  // mount
+  useEffect(() => {
+    const cachedRef = ref.current,
+      observer = new IntersectionObserver(
+        ([e]) => setIsSticky(e.intersectionRatio < 1),
+        { threshold: [1] }
+      );
+    observer.observe(cachedRef);
+    // unmount
+    return function () {
+      observer.unobserve(cachedRef);
+    };
+  }, []);
+
   return (
-    <Navbar className="bg-transparent custom_menu" expand="lg">
-      <div className="container" style={{ maxWidth: "1300px" }}>
-        <Navbar.Brand style={{ float: "left" }}>
-          <h2>SREE</h2>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse
-          style={{ width: "900px", float: "right" }}
-          id="basic-navbar-nav"
-        >
-          <Nav className="ml-auto">
-            <Router>
-              <ul className="menu_wrapper">
-                <li>
-                  <NavLink
-                    activeClassName="text-primary"
-                    to="/"
-                    className="nav_list"
-                  >
-                    Home
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    activeClassName="text-primary"
-                    to="/project"
-                    className="nav_list"
-                  >
-                    Portfolio
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    activeClassName="text-primary"
-                    to="/blog"
-                    className="nav_list"
-                  >
-                    Blog
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    activeClassName="text-primary"
-                    to="/about"
-                    className="nav_list"
-                  >
-                    About Us
-                  </NavLink>
-                </li>
-                <li>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    style={{
-                      backgroundColor: "#f9bb00",
-                      textTransform: "capitalize",
-                      width: "120px",
-                      borderRadius: "30px",
-                      fontSize: "16px",
-                      boxShadow: "0px 15px 18px #f9bb008c",
-                    }}
-                  >
-                    <Link to="contact">Contact us</Link>
-                  </Button>
-                </li>
-              </ul>
-            </Router>
-          </Nav>
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
+    <div
+      id="menu_wrapper"
+      className={className + (isSticky ? " isSticky" : "")}
+      ref={ref}
+      {...rest}
+    >
+      <Navbar className="bg-transparent custom_menu" expand="lg">
+        <div className="container" style={{ maxWidth: "1300px" }}>
+          <Navbar.Brand style={{ float: "left" }}>
+            <h2>SREE</h2>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            style={{ width: "900px", float: "right" }}
+            id="basic-navbar-nav"
+          >
+            <Nav className="ml-auto">
+              <Router>
+                <ul className="menu_wrapper">
+                  <li>
+                    <NavLink
+                      activeClassName="text-primary"
+                      to="/"
+                      className="nav_list"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      activeClassName="text-primary"
+                      to="/project"
+                      className="nav_list"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      Portfolio
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      activeClassName="text-primary"
+                      to="/blog"
+                      className="nav_list"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      Blog
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      activeClassName="text-primary"
+                      to="/about"
+                      className="nav_list"
+                      style={{ textDecoration: "none", color: "#000" }}
+                    >
+                      About Us
+                    </NavLink>
+                  </li>
+                  <li>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      style={{
+                        backgroundColor: "#f9bb00",
+                        textTransform: "capitalize",
+                        width: "120px",
+                        borderRadius: "30px",
+                        fontSize: "16px",
+                        boxShadow: "0px 15px 18px #f9bb008c",
+                      }}
+                    >
+                      <Link
+                        to="contact"
+                        style={{ textDecoration: "none", color: "#fff" }}
+                      >
+                        Contact us
+                      </Link>
+                    </Button>
+                  </li>
+                </ul>
+              </Router>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </div>
   );
 };
 
 export default NavBar;
+
+// import React from "react";
+// import { Nav, Navbar } from "react-bootstrap";
+// import { Button } from "@material-ui/core";
+// import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
+
+// const NavBar = () => {
+//   return (
+//     <Navbar className="bg-transparent custom_menu" expand="lg">
+//       <div className="container" style={{ maxWidth: "1300px" }}>
+//         <Navbar.Brand style={{ float: "left" }}>
+//           <h2>SREE</h2>
+//         </Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse
+//           style={{ width: "900px", float: "right" }}
+//           id="basic-navbar-nav"
+//         >
+//           <Nav className="ml-auto">
+//             <Router>
+//               <ul className="menu_wrapper">
+//                 <li>
+//                   <NavLink
+//                     activeClassName="text-primary"
+//                     to="/"
+//                     className="nav_list"
+//                   >
+//                     Home
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <NavLink
+//                     activeClassName="text-primary"
+//                     to="/project"
+//                     className="nav_list"
+//                   >
+//                     Portfolio
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <NavLink
+//                     activeClassName="text-primary"
+//                     to="/blog"
+//                     className="nav_list"
+//                   >
+//                     Blog
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <NavLink
+//                     activeClassName="text-primary"
+//                     to="/about"
+//                     className="nav_list"
+//                   >
+//                     About Us
+//                   </NavLink>
+//                 </li>
+//                 <li>
+//                   <Button
+//                     variant="contained"
+//                     color="primary"
+//                     style={{
+//                       backgroundColor: "#f9bb00",
+//                       textTransform: "capitalize",
+//                       width: "120px",
+//                       borderRadius: "30px",
+//                       fontSize: "16px",
+//                       boxShadow: "0px 15px 18px #f9bb008c",
+//                     }}
+//                   >
+//                     <Link to="contact">Contact us</Link>
+//                   </Button>
+//                 </li>
+//               </ul>
+//             </Router>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </div>
+//     </Navbar>
+//   );
+// };
+
+// export default NavBar;
